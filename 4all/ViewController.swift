e5fc95cc9dcbebae0e1a78dfcb9812b633f96b31
Alex Freitas <otimizalex@outlook.com>
2020-12-15 23:24:35 -0300

    Background view, stack view, label and button included.

--- 4all/ViewController.swift
+++ 4all/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
+    
+    @IBAction func actionButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
